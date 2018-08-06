0fb9f422718a0ef5bd69e20cf64c671339f4019f
Charnell Dolley <cdolley516@gmail.com>
2018-08-06 18:27:17 -0400

    adding javascript game index

---
+++ index.js
@@ -0,0 +1,37 @@
+let playGame = prompt("Do you want to play?");
+const enterName = prompt("Please enter your name.");
+
+if (playGame = 'yes' ){
+  enterName;
+}
+
+let userScore = 40;
+let grantScore = 10;
+
+function generateRandomInteger(min, max) {
+  return Math.floor(min + Math.random()*(max+1 - min))
+}
+
+//let wins = grantScore <= 0 && userScore>0;
+
+let wins = 0;
+
+while (userScore > 0 && wins < 3){
+    console.log(enterName + ` has ${userScore} health left. `);
+    console.log(`Grant has ${grantScore} health left. `);
+
+  grantScore-=generateRandomInteger(1, 2);
+  userScore-=generateRandomInteger(1, 2);
+
+
+  if (grantScore <= 0){
+      grantScore = 10;
+      console.log (enterName + " Wins");
+
+wins++;
+
+   if (grantScore > 0 && userScore<=0 && wins<3) {console.log ("Grant Wins");}
+     }
+    
+ 
+}
